2e0adda01d38aa8167a25c42da3bec69b8990d34
João Paulo Rodrigues <joaopaulo.dev@hotmail.com>
2025-05-15 20:18:31 -0300

    Update Task

--- app.py
+++ app.py
@@ -35,6 +35,26 @@ def get_task(id):
     return jsonify({"message": "Não foi possível encontrar atividade."}), 404
 
 
+@app.route('/tasks/<int:id>', methods=['PUT'])
+def update_task(id):
+    task = None
+    
+    for t in tasks:
+        if t.id == id:
+            task = t
+    
+    if task == None:
+        return jsonify({"message": "Não foi possível encontrar atividade."}), 404
+    
+    data = request.get_json()
+    task.title = data.get("title")
+    task.description = data.get("description")
+    task.completed = data.get("completed")
+    
+    return jsonify({"message": "Tarefa atualizada com sucesso."})
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
