c0556fd701935db74fa8aad51f041138a35be52e
João Paulo Rodrigues <joaopaulo.dev@hotmail.com>
2025-05-15 16:17:21 -0300

    Endpoint - create task

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.task import Task
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello World!!!"
+tasks = []
+id_task_control = 1
 
-
-@app.route("/about")
-def about():
-    return "About the page"
+@app.route("/tasks", methods=['POST'])
+def create_task():
+    global id_task_control
+    data = request.get_json()
+    newTask = Task(id=id_task_control, title=data.get("title"), description=data.get("description"))
+    id_task_control += 1    
+    tasks.append(newTask)
+    return jsonify({"message": "Nova tarefa criada com sucesso."})
 
 if __name__ == "__main__":
     app.run(debug=True)
